5f8f8fa304f36414167439ba9fc25d53f6d1077e
Ipvip57 <lpivip123@gmail.com>
2024-07-07 22:16:48 +0100

    server.mjs

--- server.js
+++
@@ -1,7 +0,0 @@
-const { PythonShell } = require('python-shell');
-require('dotenv').config();
-
-PythonShell.run('main.py', null, function (err, results) {
-  if (err) throw err;
-  console.log('Results:', results);
-});
